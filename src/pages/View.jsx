fae7c75acf2fb7bfdd64f41983cd1da16f1f5102
Sol Turipe <solcis.bio@gmail.com>
2022-02-01 15:17:19 +0100

    page view

---
+++ src/pages/View.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const View = () => {
+  return (
+    <div className="container-view">
+      <div className="img-view">
+        <img src="" alt="" />
+      </div>
+      <div className="card-view"></div>
+    </div>
+  );
+};
+
+export default View;
