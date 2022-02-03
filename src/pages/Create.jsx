6d6a922821e3aadfe2fe95f30a43a9d03faf5ec5
ClaudiaCalero <claudiaclrduro@gmail.com>
2022-02-03 13:47:00 +0100

    process create form

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "../assets/style/createForm.css"
+import "../assets/style/createForm.css";
 
 const Create = () => {
   const navigate = useNavigate();
@@ -30,7 +30,7 @@ const Create = () => {
   const handleChangeImage = (event) => {
     setImage(event.target.value);
   };
- const handleClick = (event) => {
+  const handleClick = (event) => {
     event.preventDefault();
     const create = { artist, name, description, price, image };
     fetch("http://localhost:3003/Create", {
@@ -38,63 +38,65 @@ const Create = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(create),
     }).then(() => {
-        navigate("/");
+      navigate("/");
     });
   };
   return (
-    <form>
-<div className="form-group">
-</div>
-        <div className="form-group">
-          <label for="formGroupExampleInput2">Artist</label>
+    <form className="container-Formulario">
+      <div className="container-image">
+        <img src={require("../assets/img/img-view.png")} alt="" />
+      </div>
+
+      <div className="container-form">
+        <div className="form-nft">
+          <label for="formGroupNFT">Artist</label>
           <input
             type="text"
             value={artist}
             onChange={handleChangeArtist}
-            className="form-control"
+            className="form-nft"
             placeholder="Artist"
           />
         </div>
-        <div className="form-group">
-          <label for="formGroupExampleInput2">NFT Name</label>
+        <div className="form-nft">
+          <label for="formGroupNFT">NFT Name</label>
           <input
             type="text"
             value={name}
             onChange={handleChangeName}
-            className="form-control"
+            className="form-nft"
             placeholder="Name"
           />
         </div>
-        <div className="form-group">
-          <label for="formGroupExampleInput2">Description</label>
+        <div className="form-nft">
+          <label for="formGroupNFT">Description</label>
           <input
             type="text"
             value={description}
             onChange={handleChangeDescription}
-            className="form-control"
+            className="form-nft"
             placeholder="Description"
           />
         </div>
-          <label for="formGroupExampleInput2">Price</label>
-          <input
-            type="number"
-            value={handleChangePrice}
-            onChange={handleChangePrice}
-            className="form-control"
-            placeholder="Price"
-          />
-      <div className="container">
-        <div className="form-group">
-          <label for="formGroupExampleInput2">Image</label>
-          <input
-            type="text"
-            value={image}
-            onChange={handleChangeImage}
-            className="form-control"
-            placeholder="Image"
-          />
-        </div>
-        <button type="button" className="btn btn-danger" onClick={handleClick}>
+        <label for="formGroupNFT">Price</label>
+        <input
+          type="number"
+          value={price}
+          onChange={handleChangePrice}
+          className="form-nft"
+          placeholder="Price"
+        />
+          <div className="form-nft">
+            <label for="formGroupNFT">Image</label>
+            <input
+              type="img"
+              value={image}
+              onChange={handleChangeImage}
+              className="form-nft"
+              placeholder="Image"
+            />
+          </div>
+        <button type="button" className="button" onClick={handleClick}>
           Submit
         </button>
       </div>
