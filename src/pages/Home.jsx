b5c1a20b0e863396bda16d469876d23fdd60d11b
Ceci <carbel.cecilia@gmail.com>
2022-02-03 14:37:57 +0100

    adding header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import "../App.css";
+import Header from "../components/Header";
 
 function Home() {
-  return <div className="App">coders</div>;
+  return <div className="App">
+    <Header/>
+    
+    </div>;
 }
 
 export default Home;
