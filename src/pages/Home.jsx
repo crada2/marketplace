f95cc9b212e0f235b6344b0a1596fb595758e713
Davina-dev <davinamr91@gmail.com>
2022-02-03 15:09:59 +0100

    acomodar componentes home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,9 @@ const Home = () => {
   return (
     <div>
       <Header/>
-      <Hero />
-        <div>
-        <Card />;
-        </div>
+      <Hero />  
+      <Card />;
+        
       
     </div>
   );
