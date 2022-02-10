783c663dc16a300bbb42c0c1a3caa57420663f7c
Sol Turipe <solcis.bio@gmail.com>
2022-02-10 12:30:51 +0100

    footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import "../assets/style/footer.css"
+import "../assets/style/footer.css";
 
 const Footer = () => {
   return (
     <footer className="footerContainer">
-      
-        
-        <div className="footerIcons">
-          <a className="icons1">
-            <img src={require("../assets/img/instagram.png")} alt="" width="40px"/>
-          </a>
-          <a className="icons2">
+      <div className="footerIcons">
+        <a className="icons1">
+          <img
+            src={require("../assets/img/instagram.png")}
+            alt=""
+            width="40px"
+          />
+        </a>
+        <a className="icons2">
           <img src={require("../assets/img/twitter.png")} alt="" width="40px" />
-          </a>
-          <a className="icons3">
-          <img src={require("../assets/img/facebook.png")} alt="" width="40px"/>
-          </a>
-        </div>
-        <div className="footerText">
-        <p>
-          © 2022 Crada, Inc. All rights reserved
-        </p>
-        </div>
-     
+        </a>
+        <a className="icons3">
+          <img
+            src={require("../assets/img/facebook.png")}
+            alt=""
+            width="40px"
+          />
+        </a>
+      </div>
+      <div className="footerText">
+        <p>© 2022 Crada, Inc. All rights reserved</p>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
-
